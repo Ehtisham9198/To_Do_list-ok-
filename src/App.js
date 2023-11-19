ba8466e3f664cba3af97e1e21dcc30853640b978
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2023-11-19 12:56:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [val, limi_changer]= useState('');
+  let [list, SetList]= useState([]);
+
+ 
+  const fun=(event)=>{
+  limi_changer(event.target.value);
+  }
+  const submit_req= (e)=>{
+    e.preventDefault();
+    SetList((prevList) => [...prevList, val]);
+    limi_changer('');
+     
+     }
+
+    const handleDelete = (index) => {
+      SetList((prevList) => {
+        const newList = [...prevList];
+        newList.splice(index, 1);
+        return newList;
+      });
+     }
+    
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='to'>
+    <h1 className='to-do'>To-Do List</h1>
+    <form onSubmit={submit_req}>
+    <input className='ink' type="text" placeholder=' Enter the task...' onChange={fun}  required='true' value={val}/>
+    <button className='button'>Add task</button>
+    </form>
+    <br></br>
+    <br></br>
+    <ul>
+    {list.map((e,index)=>{ 
+            return <li key={index} className='curlist'>{e} {<button >Edit</button>} {<button className='Delbut' onClick={() => handleDelete(index)}>Delete</button>}</li>
+          
+             })}
+    
+    </ul> 
+   
+  </div>
   );
 }
 
